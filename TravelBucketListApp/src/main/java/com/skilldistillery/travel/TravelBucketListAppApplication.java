f02f1d3fc4e8e9acba9f9322be2f0b5241c9d7e0
Iryna <kefa_tretinik@icloud.com>
2020-05-15 17:37:50 -0600

    Stub Boot/MVC

---
+++ TravelBucketListApp/src/main/java/com/skilldistillery/travel/TravelBucketListAppApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBucketListAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelBucketListAppApplication.class, args);
+	}
+
+}
